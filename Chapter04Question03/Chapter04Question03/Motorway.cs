b46d03405ffbc398c08e612c6b4ba821c52ba651
nruangprasert <nruangprasert@gmail.com>
2024-08-30 13:16:59 +0700

    Add files via upload

---
+++ Chapter04Question03/Chapter04Question03/Motorway.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Chapter04Question03
+{
+    internal class Motorway
+    {
+        private string nameOfTheHighway;
+        private string typeOfStreet;
+        private char direction;
+        private string surface;
+        private int numberOfLanes;
+        private bool toll;
+        private string maintainedBy;
+        public Motorway ()
+        {
+
+        }
+        public Motorway(string nameOfTheHighway, string typeOfStreet, char direction, string surface, int numberOfLanes, bool toll, string maintainedBy)
+        {
+            this.nameOfTheHighway = nameOfTheHighway;
+            this.typeOfStreet = typeOfStreet;
+            this.direction = direction;
+            this.surface = surface;
+            this.numberOfLanes = numberOfLanes;
+            this.toll = toll;
+            this.maintainedBy = maintainedBy;
+        }
+        public string NameOfTheHighway { get; set;}
+        public string TypeOfStreet { get; set;}
+        public char Direction { get; set;}
+        public string Surface { get; set;}
+        public int NumberOfLanes { get; set;}
+        public bool Toll { get; set;}
+        public string MaintainedBy { get; set;}
+        public string FullName()
+        {
+            return "\nFull name = " + nameOfTheHighway + " " + typeOfStreet + " " + direction; 
+        }
+        public string FullNameAndToll()
+        {
+            return "\n" + FullName() + "\nToll = " + toll;
+        }
+        public string FullNameAndNumberOfLanes()
+        {
+            return "\n" + FullName() + "\nNumber of lanes = " + numberOfLanes;
+        }
+        public override string ToString()
+        {
+            return "\nName of the highway : " + nameOfTheHighway +
+                    "\nType of the street : " + typeOfStreet +
+                    "\nDirection : " + direction +
+                    "\nSurface : " + surface +
+                    "\nNumber of Lanes : " + numberOfLanes +
+                    "\ntoll : " + toll +
+                    "\nMaintained by : " + maintainedBy;
+        }
+    }
+}
---
+++ Chapter04Question03/Chapter04Question03/MotorwayApp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Chapter04Question03
+{
+    internal class MotorwayApp
+    {
+        static void Main(string[] args)
+        {
+        string nameOfTheHighway = ReceiveNameOfTheHighway();
+        string typeOfStreet = ReceiveTypeOfTheStreet();
+        char direction = ReceiveDirection();
+        string surface = ReceiveSurface();
+        int numberOfLanes = ReceiveNumberOfLanes();
+        bool toll = ReceiveToll();
+        string maintainedBy = ReceiveMaintainedBy();
+
+            Motorway motorway = new Motorway(nameOfTheHighway, typeOfStreet, direction, surface, numberOfLanes, toll, maintainedBy);
+            Console.WriteLine(motorway.FullName());
+            Console.WriteLine(motorway.FullNameAndToll());
+            Console.WriteLine(motorway.FullNameAndNumberOfLanes());
+            Console.WriteLine(motorway);
+
+            Console.ReadKey();
+        }
+        static string ReceiveNameOfTheHighway()
+        {
+            Console.Write("Enter name of the highway : ");
+            return Console.ReadLine();
+        }
+        static string ReceiveTypeOfTheStreet()
+        {
+            Console.Write("Enter type of the street (i.e., Road, Street, Avenue, Blvd., Lane, etc.) : ");
+            return Console.ReadLine();
+        }
+        static char ReceiveDirection()
+        {
+            Console.Write("Enter direction (i.e., E, W, N, or S) : ");
+            return char.Parse(Console.ReadLine());
+        }
+        static string ReceiveSurface()
+        {
+            Console.Write("Enter surface (i.e., blacktop, gravel, sand, and concrete) : ");
+            return Console.ReadLine();
+        }
+        static int ReceiveNumberOfLanes()
+        {
+            Console.Write("Enter number of lanes : ");
+            return int.Parse(Console.ReadLine());
+        }
+        static bool ReceiveToll()
+        {
+            Console.Write("Enter toll payment (true or false) : ");
+            return bool.Parse(Console.ReadLine());
+        }
+        static string ReceiveMaintainedBy()
+        {
+            Console.Write("Enter party who maintain : ");
+            return Console.ReadLine();
+        }
+    }
+}
