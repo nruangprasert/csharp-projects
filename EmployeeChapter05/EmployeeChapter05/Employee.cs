0cff4209f53cb69541ce4fcdcbad49399dc3b09d
nruangprasert <nruangprasert@gmail.com>
2024-08-30 13:19:49 +0700

    Add files via upload

---
+++ EmployeeChapter05/EmployeeChapter05/Employee.cs
@@ -0,0 +1,88 @@
+﻿using System;
+namespace EmployeeChapter05
+{
+    internal class Employee
+    {
+        private string firstName;
+        private string lastName;
+        private int iD;
+        private char type;
+        private double salaryAmount;
+        private double grossPay;
+        private double hoursWorked;
+        private double hourlyPayRate;
+        private double federalTax;
+        private double socialSecurity;
+        private double retirementContribution;
+        private double totalDeductions;
+        private double takeHomePay;
+    
+        public Employee()
+        {
+
+        }
+        public Employee(string firstName, string lastName, int iD, char type, double salaryAmount)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.iD = iD;
+            this.type = type;
+            this.salaryAmount = salaryAmount;
+        }
+        public Employee(string firstName, string lastName, int iD, char type, double hoursWorked, double hourlyPayRate) 
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.iD = iD;
+            this.type = type;
+            this.hoursWorked = hoursWorked;
+            this.hourlyPayRate = hourlyPayRate;
+        }
+        public double GrossPay { get; set; }
+        public double FederalTax { get; set; }
+        public double SocialSecurity { get; set; }
+        public double RetirementContribution { get; set; }
+        public double TotalDeductions { get; set; }
+        public double TakeHomePay { get; set; }
+        public double CalculateS() 
+        {
+            return this.salaryAmount;
+        }
+        public double CalculateH()
+        {
+            if (hoursWorked > 40) 
+            {
+                grossPay = (40 * hourlyPayRate) + ((hoursWorked - 40) * (1.5 * hourlyPayRate));
+                return grossPay;
+            }
+            else
+            {
+                grossPay = (hoursWorked * hourlyPayRate);
+                return grossPay;
+            }
+        }
+        public void CalculateDeductions(double grossPay, out double federalTax, out double socialSecurity, 
+            out double retirementContribution, out double totalDeductions, out double takeHomePay) 
+        {
+            federalTax = 0.18 * grossPay;
+            socialSecurity = 0.06 * grossPay;
+            retirementContribution = 0.10 * grossPay;
+            totalDeductions = federalTax + socialSecurity + retirementContribution;
+            takeHomePay = grossPay - totalDeductions;
+        }
+        public override string ToString()
+        {
+            return "\nWeekly information for " + firstName + " " + lastName +
+                    "\n" +
+                    "\n" + string.Format("{0,-35}", "Employee ID:") + iD +
+                    "\n" + string.Format("{0,-35}", "Gross pay:") + GrossPay.ToString("C2") +
+                    "\n" + string.Format("{0,-35}", "Federal tax paid:") + FederalTax.ToString("C2") +
+                    "\n" + string.Format("{0,-35}", "Social security paid:") + SocialSecurity.ToString("C2") +
+                    "\n" + string.Format("{0,-35}", "Retirement contribution:") + RetirementContribution.ToString("C2") +
+                    "\n" + string.Format("{0,-35}", "Total deductions:") + TotalDeductions.ToString("C2") +
+                    "\n" +
+                    "\n" + string.Format("{0,-35}", "Take home pay:") + TakeHomePay.ToString("C2");
+        }
+    }
+
+}
---
+++ EmployeeChapter05/EmployeeChapter05/EmployeeApp.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace EmployeeChapter05
+{
+    internal class EmployeeApp
+    {
+        static void Main(string[] args)
+        {
+            string firstName = ReceiveFirstName();
+            string lastName = ReceiveLastName();
+            int iD = ReceiveID();
+            char type = ReceiveType();
+            CreateObject(type, firstName, lastName, iD);
+            Console.ReadKey();   
+        }
+        static void CreateObject(char type, string firstName, string lastName, int iD) 
+        {
+            if (type == 's' || type == 'S')
+            {
+                Console.Write("Please enter the salary: ");
+                double.TryParse(Console.ReadLine(), out double salaryAmount);
+                Employee employee = new Employee(firstName, lastName, iD, type, salaryAmount);
+                employee.GrossPay = employee.CalculateS();
+                employee.CalculateDeductions(employee.GrossPay, out double federalTax, out double socialSecurity,
+            out double retirementContribution, out double totalDeductions, out double takeHomePay);
+                employee.FederalTax = federalTax;
+                employee.SocialSecurity = socialSecurity;
+                employee.RetirementContribution = retirementContribution;
+                employee.TotalDeductions = totalDeductions;
+                employee.TakeHomePay = takeHomePay;
+
+                Console.WriteLine(employee);
+            }
+            else if (type == 'h' || type == 'H')
+            {
+                Console.Write("Please enter hours for the week: ");
+                double.TryParse(Console.ReadLine(), out double hoursWorked);
+                Console.Write("Please enter hourly pay rate: ");
+                double.TryParse(Console.ReadLine(), out double hourlyPayRate);
+                Employee employee = new Employee(firstName, lastName, iD, type, hoursWorked, hourlyPayRate);
+                employee.GrossPay = employee.CalculateH();
+                employee.CalculateDeductions(employee.GrossPay, out double federalTax, out double socialSecurity,
+            out double retirementContribution, out double totalDeductions, out double takeHomePay);
+                employee.FederalTax = federalTax;
+                employee.SocialSecurity = socialSecurity;
+                employee.RetirementContribution = retirementContribution;
+                employee.TotalDeductions = totalDeductions;
+                employee.TakeHomePay = takeHomePay;
+
+                Console.WriteLine(employee);
+            }
+        }
+    static string ReceiveFirstName() 
+        {
+            Console.Write("Please enter an employee first name: ");
+            return Console.ReadLine();
+        }
+        static string ReceiveLastName()
+        {
+            Console.Write("Please enter an employee last name: ");
+            return Console.ReadLine();
+        }
+        static int ReceiveID()
+        {
+            Console.Write("Please enter the employee identification number: ");
+            int.TryParse(Console.ReadLine(), out int empID);
+            return empID;
+        }
+        static char ReceiveType()
+        {
+            Console.Write("Please enter an S for salaried employee,or an H for hourly employee: ");
+            char.TryParse(Console.ReadLine(), out char empType);
+            return empType;
+        }
+
+    }
+}
