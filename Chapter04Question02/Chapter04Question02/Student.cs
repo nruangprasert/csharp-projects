17939f2c79526e87c35f4a99e8c92d920ade72eb
nruangprasert <nruangprasert@gmail.com>
2024-08-30 13:16:19 +0700

    Add files via upload

---
+++ Chapter04Question02/Chapter04Question02/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Chapter04Question02
+{
+    internal class Student
+    {
+        private int studentNumber;
+        private string firstName;
+        private string lastName;
+        private string classification;
+        private string major;
+
+        public Student()
+        {
+
+        }
+        public Student(int studentNumber, string firstName, string lastName, string classification, string major)
+        {
+            this.studentNumber = studentNumber;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.classification = classification;
+            this.major = major;
+        }
+
+        public int StudentNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Classification { get; set; }
+        public string Major { get; set; }
+
+        public override string ToString()
+        {
+            return firstName + " " + lastName;
+        }
+
+
+    }
+}
---
+++ Chapter04Question02/Chapter04Question02/StudentApp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Chapter04Question02
+{
+    internal class StudentApp
+    {
+        static void Main(string[] args)
+        {
+            int studentCount = 2;
+
+            for (int i = 1; i <= studentCount; i++)
+            {
+                int studentNumber = ReceiveInt("student number");
+                string firstName = ReceiveStr("first name");
+                string lastName = ReceiveStr("last name");
+                string classification = ReceiveStr("classification");
+                string major = ReceiveStr("major");
+
+                Student student = new Student(studentNumber, firstName, lastName, classification, major);
+                Console.WriteLine(student);
+
+                Display(firstName, lastName, major);
+
+                Console.ReadKey();
+
+            }
+        }
+        static int ReceiveInt(string value)
+        {
+            Console.Write($"Enter {value} : ");
+            return int.Parse(Console.ReadLine());
+        }
+        static string ReceiveStr(string value)
+        {
+            Console.Write($"Enter {value} : ");
+            return Console.ReadLine();
+        }
+        static void Display(string firstName, string lastname, string major)
+        {
+            Console.WriteLine($"Fullname = {firstName} {lastname} and Major = {major}");
+        }
+    }
+}
